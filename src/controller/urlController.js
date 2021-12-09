79c1d5fd835e244225616f696d6924a6649bbe7d
Mohd-Ghouse1998 <ghousemohd369@gmail.com>
2021-12-09 18:44:15 +0530

    project3

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,7 +1,52 @@
 const urlModel= require('../models/urlModel')
+const validUrl = require('valid-url');
+const shortid = require('shortid')
 
 //!-------------------functions------------------------//
-
-const urlShortner = async function (req, res) {
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
     
+    return true;
 }
+
+const baseUrl = 'http://localhost:3000'
+const urlShortner = async function (req, res) {
+    try {
+    const {longUrl}=req.body
+
+    if (!isValid(longUrl)) {
+        res.status(400).send({ status: false, message: "longUrl is required " })
+        return
+    }
+    if (!validUrl.isUri(longUrl.trim())) {
+        res.status(400).send({ status: false, message: "Invalid longUrl " })
+        return
+    }
+
+    
+
+    const urlCode = shortid.generate()
+    let url = await urlModel.findOne({longUrl})
+    if (url) {
+        res.status(400).send({status:false, messagae:" url already present"})
+    } 
+    const shortUrl = baseUrl + '/' + urlCode
+
+    let url={
+        urlCode:urlCode,
+        longUrl:longUrl,
+        shortUrl:shortUrl  
+    }
+
+    const newUrl = await urlModel.create(url)
+
+    res.status(201).send({status:true, data:newUrl})
+
+    }catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }  
+}
+
+
+module.exports.urlShortner=urlShortner
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const urlController=require("../controller/urlController")
+
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+
+router.post('/url/shorten', urlController.urlShortner)
+
 
 module.exports = router;
